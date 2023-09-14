bcb218309da346118408d5de8c4bf51c0a65f5a0
heymanav <manavp.csea19@sbjit.edu.in>
2023-09-14 21:04:28 +0530

    Added 50 Projects

---
+++ FeedbackUiDesign/script.js
@@ -0,0 +1,29 @@
+const ratings = document.querySelectorAll('.rating');
+const ratingsContainer = document.querySelector('.ratings-container');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+let selectedRating = 'Satisfied';
+
+ratingsContainer.addEventListener('click',(e) => {
+    if(e.target.parentNode.classList.contains('rating')){
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        selectedRating = e.target.nextElementSibling.innerText;
+    }
+});
+
+sendBtn.addEventListener('click',()=> {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We will use your feedback to improve customer support</p>
+    `;
+})
+
+function removeActive(){
+    for(let i = 0; i < ratings.length; i++){
+        ratings[i].classList.remove('active');
+    }
+}
